b49aedd32a53f184e4f0e68dccd0a998de8cb51c
chen2625973865 <13055936+chen2625973865@user.noreply.gitee.com>
2023-06-10 22:05:39 +0800

    完成了登录功能

---
+++ src/components/UserHome.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // 退出
+    logout () {
+      // 清空token
+      window.sessionStorage.clear()
+      // 跳转登录页
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import UserLogin from '@/components/UserLogin.vue'
+import UserHome from '@/components/UserHome.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
-
 const router = new VueRouter({
-  routes
+  routes: [
+    { path: '/', component: UserLogin },
+    { path: '/login', component: UserLogin },
+    { path: '/home', component: UserHome }
+  ]
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // next() 放行 next('/login')强制跳转
+
+  // 访问登录页 放行
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
